d787492380b02678e4435f82e900d687dd967437
BunnoBunnoMan <162572632+BunnoBunnoMan@users.noreply.github.com>
2024-03-21 18:35:29 +0100

    In Game Menu
    Added an in-game menu, bound to esc, that quits, returns to title and resumes.

--- STALKER 8-Blyat/Assets/Scripts/MenuHandler_Kris.cs
+++ STALKER 8-Blyat/Assets/Scripts/MenuHandler_Kris.cs
@@ -1,17 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MenuHandler_Kris : MonoBehaviour
 {
-    public Button button;
-
+    public GameObject pauseScreen;
+    public static bool paused = false;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        pauseScreen.SetActive(true);
+        paused = true;
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        pauseScreen.SetActive(false);
+        paused = false;
+    }
     public void Start(string sceneName)
     {
         //Debug.Log("Start");
         SceneManager.LoadScene(sceneName);
+        Time.timeScale = 1;
     }
 
     public void Quit()
