fe33fc28f24e5e57d283cea2cf69ee0fe3041a50
Tab561 <164341059+Tab561@users.noreply.github.com>
2024-03-22 11:31:24 +0100

    finished stealing code from video
    NEXT: Find out whats wrong with it and make actual code

--- STALKER 8-Blyat/Assets/Scripts/bullet_script_561.cs
+++ STALKER 8-Blyat/Assets/Scripts/bullet_script_561.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class bullet_script_561 : MonoBehaviour
@@ -16,6 +17,10 @@ public class bullet_script_561 : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
         Vector3 direction = mousePos - transform.position;
+        Vector3 rotation = transform.position - mousePos;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+        float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
     }
 
     // Update is called once per frame
