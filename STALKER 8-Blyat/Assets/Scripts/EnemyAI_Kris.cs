238ccf8612e6904d54ad7aa1beaa158c480b0dfe
BunnoBunnoMan <162572632+BunnoBunnoMan@users.noreply.github.com>
2024-04-12 01:11:31 +0200

    I have added some Enemy AI
    It's not finished, it doesn't use an FOV and it spazzes out instead of wandering.

--- STALKER 8-Blyat/Assets/Scripts/EnemyAI_Kris.cs
+++ STALKER 8-Blyat/Assets/Scripts/EnemyAI_Kris.cs
@@ -1,31 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using System.Linq;
 
 public class EnemyAi : MonoBehaviour
 {
-    public Transform enemyPos;
     public Transform playerPos;
     private Ray ray;
     private Vector2 direction;
+    public int movespeed = 1;
+    public bool seen;
+    public Rigidbody2D rb;
+    private static System.Random rng = new System.Random();
+    private Vector3 movegen;
     void Start()
     {
 
     }
-    void Update()
+    void FixedUpdate()
     {
-        direction = (playerPos.position - enemyPos.position).normalized;
-        
-        Detection();
+        direction = (playerPos.position - transform.position).normalized;
+        seen = Detection();
+        StartCoroutine(Updater());
+    }
+    IEnumerator Updater()
+    {
+        if(seen)
+        {
+            Chase();
+        }
+        else
+        {
+            Wander();
+            yield return new WaitForSecondsRealtime(1);
+        }
+        yield return null;
     }
 
-    void Detection()
+    private bool Detection()
     {
-        RaycastHit2D hitInfo = Physics2D.Raycast(enemyPos.position, direction);
-        Debug.DrawRay(enemyPos.position, direction * 50);
-        if (hitInfo.collider.gameObject.name == "Player Temp")
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, direction);
+        Debug.DrawRay(transform.position, direction * 50);
+        string[] hitList = new string[hits.Length];
+        for (int i = 0; i < hits.Length; i++)
         {
-            Debug.Log("You got pingased");
+            hitList[i] = hits[i].collider.name;
         }
+        if (hitList.Contains("Player Temp") && !hitList.Contains("Test Wall")) return true;
+        else return false;
+    }
+    void Wander()
+    {
+        movegen.x = rng.Next(-10, 10);
+        movegen.y = rng.Next(-10, 10);
+        Vector2 movement = transform.position - movegen;
+        movement = Vector2.ClampMagnitude(movement, 1);
+        rb.MovePosition(rb.position + movement * movespeed * Time.fixedDeltaTime);
+    }
+    void Chase()
+    {
+        movespeed = 3;
+        Vector2 movement = transform.position - playerPos.position;
+        movement = Vector2.ClampMagnitude(movement, 1);
+        rb.MovePosition(rb.position - movement * movespeed * Time.fixedDeltaTime);
     }
 }
