f18ab0361a02438ed058e92e5fd64ae010dac1ad
BunnoBunnoMan <162572632+BunnoBunnoMan@users.noreply.github.com>
2024-04-12 23:04:34 +0200

    ENEMY AI OUT OF ALPHA
    just needs some polishing

---
+++ STALKER 8-Blyat/Assets/Scripts/Detection.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public class Detection : MonoBehaviour
+{
+    public bool seen;
+    private Ray ray;
+    private Vector2 direction;
+    public Transform playerPos;
+    public static bool Detect(Transform playerPos, Transform enemyPos)
+    {
+        Vector2 direction = (playerPos.position - enemyPos.position).normalized;
+        RaycastHit2D[] hits = Physics2D.RaycastAll(enemyPos.position, direction);
+        Debug.DrawRay(enemyPos.position, direction * 50);
+        string[] hitList = new string[hits.Length];
+        for (int i = 0; i < hits.Length; i++)
+        {
+            hitList[i] = hits[i].collider.tag;
+        }
+        if (!hitList.Contains("Walls")) return true;
+        else return false;
+    }
+}
--- STALKER 8-Blyat/Assets/Scripts/EnemyAI_Kris.cs
+++ STALKER 8-Blyat/Assets/Scripts/EnemyAI_Kris.cs
@@ -7,41 +7,56 @@ using System.Threading.Tasks;
 public class EnemyAi : MonoBehaviour
 {
     public Transform playerPos;
-    public int movespeed = 1;
+    public int movespeed;
     public Rigidbody2D rb;
     private static System.Random rng = new System.Random();
     private Vector3 movegen;
     private bool seen;
-    private void FixedUpdate()
+    public Transform enemyPos;
+    private bool wanderGened;
+    public GameObject waypoint;
+    private Vector2 movement;
+    void Update()
     {
-        seen = Detection
-        if()
+        seen = Detection.Detect(playerPos, enemyPos);
+    }
+    void FixedUpdate()
+    {
+        if(seen)
         {
-            Chase();
+            wanderGened = false;
+            movespeed = 3;
+            movement = transform.position - playerPos.position;
+            movement = Vector2.ClampMagnitude(movement, 1);
+            rb.MovePosition(rb.position - movespeed * Time.fixedDeltaTime * movement);
         }
         else
         {
-        movegen.x = rng.Next(-10, 10);
-        movegen.y = rng.Next(-10, 10);
-        Debug.Log($"{movegen.x}.{movegen.y}");
-        
+            if(wanderGened == false)
+            {
+                movegen = WanderGen();
+                Debug.Log($"{movegen.x}.{movegen.y}");
+                Instantiate(waypoint, movegen, Quaternion.identity);
+                movespeed = 1;
+                wanderGened = true;
+            }
+            if(Vector2.Distance(enemyPos.position, movegen) > 0.5)
+            {
+                Debug.Log(Vector2.Distance(enemyPos.position, movegen));
+                movement = enemyPos.position - movegen;
+                movement = Vector2.ClampMagnitude(movement, 1);
+                rb.MovePosition(rb.position - movespeed * movement * Time.fixedDeltaTime);
+            }
+            else if(Vector2.Distance(enemyPos.position, movegen) < 0.5) wanderGened = false;
+
         }
-        
     }
-    private void Wander(Vector3 enemyPos)
+    private Vector3 WanderGen()
     {
-        while (Vector2.Distance(enemyPos, movegen) != 0)
-        {
-            Vector3 movement = enemyPos - movegen;
-            movement = Vector2.ClampMagnitude(movement, 1);
-            rb.MovePosition(enemyPos + movespeed * Time.fixedDeltaTime * movement);
-        }
-    }
-    void Chase()
-    {
-        movespeed = 3;
-        Vector2 movement = transform.position - playerPos.position;
-        movement = Vector2.ClampMagnitude(movement, 1);
-        rb.MovePosition(rb.position - movespeed * Time.fixedDeltaTime * movement);
+        int xMax = rng.Next(-5, 5);
+        int yMax = rng.Next(-5, 5);
+        movegen.x = transform.position.x + xMax;
+        movegen.y = transform.position.y + yMax;
+        return movegen;
     }
 }
