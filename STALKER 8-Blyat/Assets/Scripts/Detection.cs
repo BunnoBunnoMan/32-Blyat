92767af0e02d1c0ee9c25877812847dc7f6c0395
BunnoBunnoMan <162572632+BunnoBunnoMan@users.noreply.github.com>
2024-04-25 14:52:51 +0200

    FOV done
    Exile Vilify

--- STALKER 8-Blyat/Assets/Scripts/Detection.cs
+++ STALKER 8-Blyat/Assets/Scripts/Detection.cs
@@ -5,16 +5,21 @@ using System.Linq;
 using System;
 using UnityEngine.Rendering;
 using JetBrains.Annotations;
+using Unity.VisualScripting;
+using Unity.Mathematics;
 
 public class Detection : MonoBehaviour
 {
     public bool seen;
     public Transform position;
+    public Transform holderPosition;
     public float fov;
     public int levelOfDetail;
     public float viewDistance;
     public LayerMask layerMask;
     private Mesh mesh;
+    public MeshCollider collision;
+    public GameObject fovHolder;
     /*public static bool Detect(Vector3 playerPos, Transform enemyPos, float distance)
     {
         Vector2 direction = (playerPos - enemyPos.position).normalized;
@@ -32,54 +37,14 @@ public class Detection : MonoBehaviour
     void Start()
     {
         mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
-        /*float angle = 0f;
-        float angleIncrease = fov / levelOfDetail;
-
-        Vector3[] vertices = new Vector3[levelOfDetail + 2];
-        int[] triangles = new int[levelOfDetail * 3];
-        vertices[0] = Vector3.zero;
-
-        int vertexIndex = 1;
-        int triangleIndex = 0;
-        for (int i = 0; i <= levelOfDetail; i++)
-        {
-            Vector3 vertex;
-            RaycastHit2D hit = Physics2D.Raycast(position.position, VectorFromAngle(angle), viewDistance, layerMask);
-            //Debug.DrawRay(position.position, VectorFromAngle(angle), Color.white, viewDistance);
-            if(hit.collider == null)
-            {
-                vertex = Vector3.zero + VectorFromAngle(angle) * viewDistance;
-                Debug.Log("No Hit:" + vertex);
-            }
-            else
-            {
-                vertex = hit.point;
-                Debug.Log("Hit:" + hit.point);
-            }
-            vertices[vertexIndex] = vertex;
-            if(i > 0)
-            {
-                triangles[triangleIndex + 2] = 0;
-                triangles[triangleIndex + 1] = vertexIndex - 1;
-                //Debug.Log("vertexindex:" + vertices[vertexIndex - 1]);
-                triangles[triangleIndex] = vertexIndex;
-                //Debug.Log("vertexindex - 1:" + vertices[vertexIndex]);
-                triangleIndex += 3;
-            }
-            vertexIndex++;
-            angle -= angleIncrease;
-        }
-
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;*/
+        fovHolder.GetComponent<MeshFilter>().mesh = mesh;
+        fovHolder.GetComponent<MeshCollider>().sharedMesh = mesh;
         
     }
     void Update()
     {
-        float angle = 0f;
+        float rayAngle = holderPosition.eulerAngles.z;
         float angleIncrease = fov / levelOfDetail;
-
         Vector3[] vertices = new Vector3[levelOfDetail + 2];
         int[] triangles = new int[levelOfDetail * 3];
         vertices[0] = Vector3.zero;
@@ -89,31 +54,28 @@ public class Detection : MonoBehaviour
         for (int i = 0; i <= levelOfDetail; i++)
         {
             Vector3 vertex;
-            RaycastHit2D hit = Physics2D.Raycast(position.position, VectorFromAngle(angle), viewDistance, layerMask);
-            //Debug.DrawRay(position.position, VectorFromAngle(angle), Color.white, viewDistance);
+            RaycastHit2D hit = Physics2D.Raycast(position.position, VectorFromAngle(rayAngle), viewDistance, layerMask);
+            Debug.DrawRay(position.position, Vector3.zero + VectorFromAngle(rayAngle) * viewDistance);
             if(hit.collider == null)
             {
-                vertex = Vector3.zero + VectorFromAngle(angle) * viewDistance;
-                Debug.Log(vertex);
+                vertex = Vector3.zero + VectorFromAngle(rayAngle) * viewDistance;
             }
             else
             {
                 Vector3 point = hit.point;
-                vertex = point - transform.position;
-                Debug.Log(hit.point);
+                vertex = point - position.position;
+                //Debug.Log(vertex);
             }
             vertices[vertexIndex] = vertex;
             if(i > 0)
             {
                 triangles[triangleIndex] = 0;
                 triangles[triangleIndex + 1] = vertexIndex - 1;
-                Debug.Log("vertexindex:" + vertices[vertexIndex - 1]);
                 triangles[triangleIndex + 2] = vertexIndex;
-                Debug.Log("vertexindex - 1:" + vertices[vertexIndex]);
                 triangleIndex += 3;
             }
             vertexIndex++;
-            angle -= angleIncrease;
+            rayAngle -= angleIncrease;
         }
 
         mesh.vertices = vertices;
@@ -123,5 +85,10 @@ public class Detection : MonoBehaviour
     {
         float angleRad = angle * Mathf.Deg2Rad;//(Mathf.PI /180f);
         return new Vector3(Mathf.Cos(angleRad),Mathf.Sin(angleRad));
+    }
+
+    void OnTriggerEnter2D(Collider2D collision2)
+    {
+        Debug.Log(collision2.gameObject.name);
     }
 }
