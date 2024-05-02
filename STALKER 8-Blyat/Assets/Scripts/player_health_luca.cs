530c9bd86ca48eda89bf834832ee1013418f326b
Tab561 <164341059+Tab561@users.noreply.github.com>
2024-05-02 14:12:06 +0200

    Update player_health_luca.cs

--- STALKER 8-Blyat/Assets/Scripts/player_health_luca.cs
+++ STALKER 8-Blyat/Assets/Scripts/player_health_luca.cs
@@ -15,9 +15,10 @@ public class Player_health_luca : MonoBehaviour
   }
   
 
-  // void Update(){
-  //   if ()
-  // }
+  void Update(){
+    health_bar.SetHealth(currentHealth);
+    //When enemy scripts are finished, implement some system that properly applies damage depending on the enemy that attacks.
+  }
 
   void TakeDamage(int damage){
     currentHealth -= damage;
