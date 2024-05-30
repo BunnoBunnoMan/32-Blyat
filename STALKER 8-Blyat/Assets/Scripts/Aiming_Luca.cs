08c72ef7f63892ac63bc11d6abfcc9e31ae86b40
Tab561 <164341059+Tab561@users.noreply.github.com>
2024-05-30 13:04:41 +0200

    Update Aiming_Luca.cs

--- STALKER 8-Blyat/Assets/Scripts/Aiming_Luca.cs
+++ STALKER 8-Blyat/Assets/Scripts/Aiming_Luca.cs
@@ -15,12 +15,12 @@ public class Aiming : MonoBehaviour
     public GunStats Stats;
     public bool loaded;
 
+    
+
     // Start is called before the first frame update
     void Start()
     {   
-
         cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-      
     }
 
     // Update is called once per frame
