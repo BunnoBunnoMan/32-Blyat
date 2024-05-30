364fa32d884e4529817e1787d31272d0abd115b1
Tab561 <164341059+Tab561@users.noreply.github.com>
2024-05-30 12:38:31 +0200

    adapted some of my scripts to the Cherger name change

--- STALKER 8-Blyat/Assets/Scripts/Chaser_Health_Luca.cs
+++ STALKER 8-Blyat/Assets/Scripts/Chaser_Health_Luca.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Charger_Health_Luca : MonoBehaviour
+public class Chaser_Health_Luca : MonoBehaviour
 {
   public int maxHealth;
   public int currentHealth;
